f251764599bc098f6a6bcae5ccbffcb2159dc5d4
chelsie mahoney <chelsiesindar@gmail.com>
2022-03-06 10:57:40 -0700

    notes again

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const express = require("express");
 
+   
+const PORT = process.env.PORT || 3001;
+const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({
+    extended: true
+}));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.static("./public/"));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
